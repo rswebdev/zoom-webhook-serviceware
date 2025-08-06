fc0f2fe0702f15e51fe2858bf3b4e9306ef4731c
robert.schmidt04@telekom.de <robert.schmidt04@telekom.de>
2025-08-06 11:21:20 +0200

    chore: update devDependencies for ESLint and related packages

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJest from 'eslint-plugin-jest';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    extends: compat.extends('eslint:recommended'),
+
+    plugins: { jest: pluginJest },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...pluginJest.environments.globals.globals,
+      },
+
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  },
+]);
