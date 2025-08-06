34ff6ac3b72c384fb7839e96605cc9c325ed2ff5
robert.schmidt04@telekom.de <robert.schmidt04@telekom.de>
2025-08-06 10:19:44 +0200

    chore: update devDependencies to include @babel/preset-env and babel-jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
+  testMatch: ['**/tests/**/*.test.[cm]*js'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
+  transform: {
+    '^.+\\.mjs$': 'babel-jest',
+  },
 };
